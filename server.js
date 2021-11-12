70a5f6bfb6980d16e2d7df6bdcb8040a534b68e6
ahmedparbes <fbhelpsbd@gmail.com>
2021-11-12 11:58:45 +0600

    add

--- server.js
+++ server.js
@@ -109,6 +109,21 @@ async function run() {
             const query = { _id: ObjectId(id) }
             const result = await userCollection.deleteOne(query)
             res.send(result);
+        });
+
+        app.put('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedUser = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    status: updatedUser.name,
+                    note: updatedUser.note
+                },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options)
+            res.json(result)
         })
 
         //Rating
@@ -124,40 +139,6 @@ async function run() {
             const result = await cursor.toArray()
             res.send(result);
         })
-
-        // // Make Admin
-
-
-        // app.get('/users/:email', async (req, res) => {
-        //     const email = req.params.email;
-        //     const query = { email: email };
-        //     const user = await usersCollection.findOne(query);
-        //     let isAdmin = false;
-        //     if (user?.role === 'admin') {
-        //         isAdmin = true;
-        //     }
-        //     res.json({ admin: isAdmin });
-        // })
-
-
-
-        // app.put('/users/admin', verifyToken, async (req, res) => {
-        //     const user = req.body;
-        //     const requester = req.decodedEmail;
-        //     if (requester) {
-        //         const requesterAccount = await usersCollection.findOne({ email: requester });
-        //         if (requesterAccount.role === 'admin') {
-        //             const filter = { email: user.email };
-        //             const updateDoc = { $set: { role: 'admin' } };
-        //             const result = await usersCollection.updateOne(filter, updateDoc);
-        //             res.json(result);
-        //         }
-        //     }
-        //     else {
-        //         res.status(403).json({ message: 'you do not have access to make admin' })
-        //     }
-
-        // })
 
 
         app.listen(port, () => {
